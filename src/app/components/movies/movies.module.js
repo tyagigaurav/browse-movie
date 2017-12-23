e6d092ce8df2d7cca7280669cafbcd7d27480766
Gaurav Tyagi <gt.gaurav1992@gmail.com>
2017-12-24 01:54:26 +0530

    added movie module

---
+++ src/app/components/movies/movies.module.js
@@ -0,0 +1,12 @@
+(function () {
+
+    'use strict';
+
+    var angular = require('angular');
+    
+    var ComponentsModule = angular.module('app.components', [
+    ]);
+
+    module.exports = ComponentsModule.name;
+
+})();
